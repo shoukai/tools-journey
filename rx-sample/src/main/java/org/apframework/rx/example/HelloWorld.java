d41a3f1b58d3765bf4fbc3016edf8a973ab86a27
Shoukai Huang <huangshoukai@yeah.net>
2018-01-15 23:58:24 +0800

    add rx demo

---
+++ rx-sample/src/main/java/org/apframework/rx/example/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.apframework.rx.example;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        Flowable.just("Hello world").subscribe(System.out::println);
+    }
+}
