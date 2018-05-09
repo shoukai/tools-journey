ec284fd3b98efe9ce2687ce5d48d56d7f33f1fca
黄守凯 <huangshoukai@renrenche.com>
2018-05-09 23:32:26 +0800

    add spring boot solr sample

---
+++ spring-data-solr-sample/src/main/java/org/apframework/data/solr/Product.java
@@ -0,0 +1,19 @@
+package org.apframework.data.solr;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.solr.core.mapping.Indexed;
+import org.springframework.data.solr.core.mapping.SolrDocument;
+
+@Data
+@SolrDocument(solrCoreName = "product")
+public class Product {
+
+    @Id
+    @Indexed(name = "id", type = "string")
+    private String id;
+
+    @Indexed(name = "name", type = "string")
+    private String name;
+
+}
