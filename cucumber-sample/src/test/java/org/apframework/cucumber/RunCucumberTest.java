5390096f0e4a918084039b25ce1a8b24f5597a66
Shoukai Huang <huangshoukai@renrenche.com>
2019-07-01 19:35:04 +0800

    fix cucumber sample

--- cucumber-sample/src/test/java/org/apframework/cucumber/RunCucumberTest.java
+++ cucumber-sample/src/test/java/org/apframework/cucumber/RunCucumberTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"progress", "json:target/cucumber-report.json"})
-public class RunCukesTest {
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {
 }
---
+++ cucumber-sample/src/test/java/org/apframework/cucumber/Stepdefs.java
@@ -0,0 +1,34 @@
+package org.apframework.cucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
+
+public class Stepdefs {
+    private String today;
+    private String actualAnswer;
+
+    @Given("^today is \"([^\"]*)\"$")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("^I ask whether it's Friday yet$")
+    public void i_ask_whether_it_s_Friday_yet() {
+        this.actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("^I should be told \"([^\"]*)\"$")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
+
