aec79967d49d24d7e184c41ba2c1c9e60e2ded26
Shoukai Huang <huangshoukai@renrenche.com>
2020-06-19 00:16:19 +0800

    Create CacheTest.java

---
+++ guava-sample/src/test/java/org/apframework/guava/cache/CacheTest.java
@@ -0,0 +1,84 @@
+package org.apframework.guava.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import lombok.Data;
+
+public class CacheTest {
+
+    public static void main(String args[]) {
+        //create a cache for employees based on their employee id
+        LoadingCache<String, Employee> employeeCache =
+                CacheBuilder.newBuilder()
+                        .maximumSize(100) // maximum 100 records can be cached
+                        .expireAfterAccess(30, TimeUnit.MINUTES) // cache will expire after 30 minutes of access
+                        .build(new CacheLoader<String, Employee>() {
+                            // build the cacheloader
+                            @Override
+                            public Employee load(String empId) throws Exception {
+                                //make the expensive call
+                                return getFromDatabase(empId);
+                            }
+                        });
+
+        try {
+            //on first invocation, cache will be populated with corresponding
+            //employee record
+            System.out.println("Invocation #1");
+            System.out.println(employeeCache.get("100"));
+            System.out.println(employeeCache.get("103"));
+            System.out.println(employeeCache.get("110"));
+            //second invocation, data will be returned from cache
+            System.out.println("Invocation #2");
+            System.out.println(employeeCache.get("100"));
+            System.out.println(employeeCache.get("103"));
+            System.out.println(employeeCache.get("110"));
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Employee getFromDatabase(String empId) {
+        Employee e1 = new Employee("Mahesh", "Finance", "100");
+        Employee e2 = new Employee("Rohan", "IT", "103");
+        Employee e3 = new Employee("Sohan", "Admin", "110");
+
+        Map<String,Employee> database = new HashMap<>();
+        database.put("100", e1);
+        database.put("103", e2);
+        database.put("110", e3);
+        System.out.println("Database hit for" + empId);
+        return database.get(empId);
+    }
+}
+
+@Data
+class Employee {
+    String name;
+    String dept;
+    String emplD;
+
+    public Employee(String name, String dept, String empID) {
+        this.name = name;
+        this.dept = dept;
+        this.emplD = empID;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(Employee.class)
+                .add("Name", name)
+                .add("Department", dept)
+                .add("Emp Id", emplD).toString();
+    }
+}
+
+
